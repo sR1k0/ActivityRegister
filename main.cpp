e771180f8764e2454dfad7d4367cb248efa7aab2
enriko <enriko.uku@stud.unifi.it>
2023-08-04 06:26:30 +0200

    aggiunto libreria Qt e creato una finestra di prova

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <QtWidgets>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    QWidget window;
+    window.resize(320,240);
+    window.show();
+    window.setWindowTitle(QApplication::translate("journal", "Activity Register"));
+    return app.exec();
+
 }
